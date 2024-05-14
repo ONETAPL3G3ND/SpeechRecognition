ab77f0ee21a5696b0d6af909a5b54250de34cd4e
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-14 21:54:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+import pyaudio
+
+
+recognizer = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Скажи что-нибудь...")
+    audio = recognizer.listen(source)
+try:
+    text = recognizer.recognize_google(audio, language="ru-RU")
+    print(f"Распознанная речь: {text}")
+except sr.RequestErroг:
+    print("Ошибка при запросе к API Google")
+except sr.UnknownValueError:
+    print("Не удалось распознать речь")
